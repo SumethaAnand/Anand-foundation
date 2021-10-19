3d4e8d4ad3c9e9fe1287abb9ac2a5cfaaf11200d
SumethaAnand <87773805+SumethaAnand@users.noreply.github.com>
2021-10-19 13:52:34 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"/>
+    <link rel="stylesheet" href="sucess.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <title>About</title>
+  </head>
+  <body>
+    <?php  include 'partials/navbar.php' ;  ?>
+    
+<h2>To empower people and enable them to help themselves <br>and their communities. Short-term help is great<br> but long-term stability is the goal. Our projects are <br>developed with the hope of long-term <br>growth for communities all over Madurai.</h2>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+<style>
+    body{
+background-image:url(images/children-banner.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+     }
+    
+   h2{
+     margin-top:400px;
+     text-shadow: 3px 3px #DBDCC1 ;
+      font-size:40px;
+       text-align:center;
+       font-style:bold;
+     color:#000000;}
+   }
+</style>
+
+
+  </body>
+</html>
